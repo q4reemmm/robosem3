8c3d212922f41363565fb8ccfd0fdc399b9260f1
eupcyx <hafidhpasha098@gmail.com>
2026-09-09 11:47:11 +0700

    feat(m01): add Dockerized ROS 2 environment

---
+++ src/my_first_robot_package/my_first_robot_package/hello_robot.py
@@ -0,0 +1,6 @@
+def main():
+    print('Hi from my_first_robot_package.')
+
+
+if __name__ == '__main__':
+    main()
---
+++ src/my_first_robot_package/setup.py
@@ -0,0 +1,30 @@
+from setuptools import find_packages, setup
+
+package_name = 'my_first_robot_package'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='dev',
+    maintainer_email='dev@todo.todo',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    extras_require={
+        'test': [
+            'pytest',
+        ],
+    },
+    entry_points={
+        'console_scripts': [
+            'hello_robot = my_first_robot_package.hello_robot:main'
+        ],
+    },
+)
